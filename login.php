ffd1a7484c39835a8d5f4ec6bd365676ba41113a
elleivan <elleivan@greeny.cs.tlu.ee>
2016-09-15 13:07:53 +0300

    updated login page

--- login.php
+++ login.php
@@ -1,3 +1,56 @@
+<?php
+
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+
+	
+	$signupEmailError = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST["signupEmail"])) {
+		
+		// oli olemas, ehk keegi vajutas nuppu
+		//kas oli tühi
+		if (empty ($_POST["signupEmail"])) {
+			
+			//oli tõesti tühi
+			$signupEmailError = "See väli on kohustuslik";
+			
+		}
+	
+	}
+	
+
+	$signupPasswordError = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST["signupPassword"])) {
+		
+		// oli olemas, ehk keegi vajutas nuppu
+		//kas oli tühi
+		if (empty ($_POST["signupPassword"])) {
+			
+			//oli tõesti tühi
+			$signupPasswordError = "See väli on kohustuslik";
+			
+		} else {
+			
+			//oli midagi, ei olnud tühi
+			
+			//kas pikkus vähemalt 8
+			if (strlen ($_POST["signupPassword"]) < 8 ) {
+				
+				$signupPasswordError = "Parool peab olema vähemalt 8 tm pikk";
+				
+			}
+			
+		}
+	
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,17 +61,47 @@
 		<h1>Logi sisse</h1>
 		
 		
-		<form>
-		
-			<input type="email">
+		<form method="POST">
+			
+			<input placeholder="E-mail" name="loginEmail" type="email">
 			
 			<br><br>
 			
-			<input type="password">
+			<input placeholder="Parool" name="loginPassword" type="password">
+			
 			
 			<br><br>
 			
 			<input type="submit">
+			
+		
+		</form>
+
+	</body>
+</html>
+
+
+<html>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
+
+		<h1>Loo kasutaja</h1>
+		
+		
+		<form method="POST">
+			<label>E-mail</label><br>
+			<input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+			<label>Parool</label><br>
+			<input name="signupPassword" type="password"> <?php echo $signupPasswordError; ?>
+			
+			
+			<br><br>
+			
+			<input type="submit" value="Loo kasutaja">
 			
 		
 		</form>
